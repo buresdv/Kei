d46e6dc605d91472c160f3dff7a8e7340878b934
David Bureš <buresdv@gmail.com>
2023-11-01 20:33:07 +0100

    Initial Commit

---
+++ Klic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Klic
+//
+//  Created by David Bureš on 01.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Klic/KlicApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlicApp.swift
+//  Klic
+//
+//  Created by David Bureš on 01.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct KlicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
